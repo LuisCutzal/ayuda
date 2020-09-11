95a408a18c978c5584bf4de4561e0efcb3350759
LuisCutzal <2842258270404@ingenieria.usac.edu.gt>
2020-09-10 23:12:24 -0600

    primero

---
+++ IPC2_201700841/IPC2_201700841/Controllers/ArchivoCargaController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace IPC2_201700841.Controllers
+{
+    public class ArchivoCargaController : Controller
+    {
+        
+        // GET: FileUpload
+        public ActionResult Index()
+        {
+            //var items = GetFiles();
+            //return View(items);
+            
+            return View();
+        }
+
+        
+    }
+}
